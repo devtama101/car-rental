faff1cbc23b78b336c91197fed2c11e607457eb8
Tama Dev <dev.tama101@gmail.com>
2026-05-21 22:33:19 +0700

    debug: bare-bones custom login - no overrides

--- app/Filament/Auth/Login.php
+++ app/Filament/Auth/Login.php
@@ -2,24 +2,9 @@
 
 namespace App\Filament\Auth;
 
-use Filament\Auth\Http\Responses\Contracts\LoginResponse;
 use Filament\Auth\Pages\Login as BaseLogin;
-use Illuminate\Contracts\Support\Htmlable;
 
 class Login extends BaseLogin
 {
-    public function authenticate(): ?LoginResponse
-    {
-        return parent::authenticate();
-    }
-
-    public function getTitle(): string | Htmlable
-    {
-        return __('Masuk');
-    }
-
-    public function getHeading(): string | Htmlable | null
-    {
-        return __('Masuk ke Akun Anda');
-    }
+    //
 }
